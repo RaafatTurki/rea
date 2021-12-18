6674094fd1904d5cca4cbe2b83d1761b32c414c9
Raafat Turki <raafat.turki@pm.me>
2021-12-18 23:10:14 +0300

    updated Img

--- src/lib/Img.jsx
+++ src/lib/Img.jsx
@@ -4,10 +4,23 @@ import './Img.css'
 
 export default class Img extends React.Component {
 
+    state = {
+        is_loaded: false
+    }
+
+    handle_load = () => {
+        this.setState({ is_loaded: true })
+    }
+
     render() {
         return (
-            <div>
-                <img src={this.props.src} alt={this.props.alt} />
+            <div className='image_container'>
+                <img
+                    src={this.props.src}
+                    alt={this.props.alt}
+                    className={this.state.is_loaded ? "is_loaded" : null}
+                    onLoad={this.handle_load}
+                />
             </div>
         )
     }

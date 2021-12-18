ffd3ca80fe5c06f4baae9e233ee215844ec2af1c
Raafat Turki <raafat.turki@pm.me>
2021-12-18 23:09:49 +0300

    added NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
-import './App.css'
-// import Nav from 'lib/Nav'
-import Home from 'pages/Home'
-// import NotFound from 'pages/NotFound'
-import {BrowserRouter,Routes, Route} from 'react-router-dom'
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
-function App() {
-    const routes = [
+import Home from 'pages/Home'
+import NotFound from 'pages/NotFound'
+
+import './App.css'
+
+export default class App extends React.Component {
+    routes = [
         {
             path: "/",
             component: <Home/>,
         },
-        // {
-        //   path: "*",
-        //   component: <NotFound/>,
-        // }
+        {
+          path: "*",
+          component: <NotFound/>,
+        }
     ]
 
-    return (
-        <BrowserRouter>
-            <Routes>
-                {routes.map((r, i) => <Route path={r.path} element={r.component} key={i}/>)}
-            </Routes>
-        </BrowserRouter>
-    )
+    render() {
+        return (
+            <BrowserRouter>
+                <Routes>
+                    {this.routes.map((r, i) => <Route path={r.path} element={r.component} key={i}/>)}
+                </Routes>
+            </BrowserRouter>
+        )
+    }
 }
-export default App
